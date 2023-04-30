1589327246e0ec17ee5f1218675895c8f5956e06
Jonathan-Beer-YU <jbeer2@mail.yu.edu>
2023-04-30 14:36:51 -0400

    updated files

--- internetworking.py
+++ internetworking.py
@@ -1,5 +1,13 @@
 from .localstore import NodeBody
 
+#gets the nodebody from a given IP. 
 def requestInfo(IP:str) -> NodeBody:
     #return stuff about IP, implement later
     return NodeBody()
+
+#encrypts the bytes with the given IP's private key. 
+def handshake(IP:str, b:bytes):
+    pass
+
+def getNeighbors(IP:str):
+    return requestInfo(IP).neighbors
