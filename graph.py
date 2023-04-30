88736688336285807f50c73d826982b3586ac520
veredg1 <132212651+veredg1@users.noreply.github.com>
2023-04-30 11:05:28 -0400

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,23 @@
+import rsa
+import networkx
+
+
+class Node():
+
+    def Node(self, name, label, supply, demand):
+        self._name = name  # name of the connection
+        self._label = label  # is it a producer or a supplier? 
+        self._neighbors = [None]
+        self._need = need
+        self._want = want
+        self._privateKey = 0
+        self._publicKey = 0  # default values for now
+
+
+
+
+
+
+
+
+
